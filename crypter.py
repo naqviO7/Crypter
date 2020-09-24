1997b9553a9248ed02d70d56c0f6edc3194a3836
Adrijan <45051889+adrijano@users.noreply.github.com>
2020-09-24 15:57:44 +0200

    update

---
+++ crypter.py
@@ -0,0 +1,161 @@
+import PySimpleGUI as sg
+import os
+import base64
+import webbrowser
+from json import (load as jsonload, dump as jsondump)
+from os import path
+import json
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
+
+
+SETTINGS_FILE = path.join(path.dirname(__file__), r'settings_file.cfg')
+DEFAULT_SETTINGS = {'theme': sg.theme()}
+SETTINGS_KEYS_TO_ELEMENT_KEYS = {'theme': '-THEME-'}
+
+def load_settings(settings_file, default_settings):
+    try:
+        with open(settings_file, 'r') as f:
+            settings = jsonload(f)
+    except Exception as e:
+        sg.popup_quick_message(f'exception {e}', 'No settings file found... will create one for you', keep_on_top=True, background_color='red', text_color='white')
+        settings = default_settings
+        save_settings(settings_file, settings, None)
+    return settings
+
+
+def save_settings(settings_file, settings, values):
+    if values:      
+        for key in SETTINGS_KEYS_TO_ELEMENT_KEYS:  
+            try:
+                settings[key] = values[SETTINGS_KEYS_TO_ELEMENT_KEYS[key]]
+            except Exception as e:
+                print(f'Problem updating settings from window values. Key = {key}')
+
+    with open(settings_file, 'w') as f:
+        jsondump(settings, f)
+
+    sg.popup('Settings saved')
+
+def create_settings_window(settings):
+    sg.theme(settings['theme'])
+
+    def TextLabel(text): return sg.Text(text+':', justification='r', size=(15,1))
+
+    layout = [  [sg.Text('Settings', font='Any 15')],
+                [TextLabel('Theme'),sg.Combo(sg.theme_list(), size=(20, 20), key='-THEME-')],
+                [sg.Button('Save'), sg.Button('Exit')]  ]
+
+    window = sg.Window('Settings', layout, keep_on_top=True, finalize=True)
+
+    for key in SETTINGS_KEYS_TO_ELEMENT_KEYS:
+        try:
+            window[SETTINGS_KEYS_TO_ELEMENT_KEYS[key]].update(value=settings[key])
+        except Exception as e:
+            print(f'Problem updating PySimpleGUI window from settings. Key = {key}')
+
+    return window
+
+
+
+
+        
+def create_main_window(settings):
+    sg.theme(settings['theme'])
+    menu_def = [['&Menu', ['&Settings', 'E&xit']],
+                ['&Help', '&About...']]
+
+    right_click_menu = ['Unused', ['&Copy', '&Paste','Settings', 'E&xit']]
+
+    layout =  [[sg.Menu(menu_def)],
+               [sg.Image('enc.png'),sg.Text('Encrypt and decrypt files', size=[32, 1]), sg.Button('', key='paypal', size=(12,1), button_color=(sg.theme_background_color(), sg.theme_background_color()),
+                                                    image_filename='paypal.png', image_size=(80, 50), image_subsample=2, border_width=0)],      
+               [sg.Output(size=(60, 20), key='out')],
+               [sg.Text('Password',size=(10,1)),sg.In(size=(49,1), key=('pass'))],
+               [sg.Text('Select file', size=(10,1)),sg.In(size=(40, 1),key='-in-'), sg.FileBrowse()],
+               [sg.Button('Encrypt'), sg.Button('Decrypt')]]
+
+    return sg.Window('Crypter', default_element_size=(30, 2)).Layout(layout)      
+
+def main():
+    window, settings = None, load_settings(SETTINGS_FILE, DEFAULT_SETTINGS )
+    while True:
+        if window is None:
+            window = create_main_window(settings)
+
+        event, value = window.Read()
+        filename = value['-in-']
+        passw = value['pass']
+        if event == 'Encrypt':
+            try:
+                with open(filename, 'rb') as f:
+                    data = f.read()
+                salt = os.urandom(32)
+                kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=100000, backend=default_backend())
+                password = passw.encode()
+                file = filename.encode()
+                key = base64.urlsafe_b64encode(kdf.derive(password))
+                c = key.decode()
+                fernet = Fernet(key)
+                encrypted = fernet.encrypt(data)
+                file = open('key.docx', 'rb')
+                keyt = file.read() 
+                file.close()
+                with open(filename, 'wb') as f:
+                    f.write(encrypted)
+                with open("key.docx", "wb") as ekey:
+                    ekey.write(key)
+
+                print('File encrypted...\n')
+                print("Key is stored in key.docx file. Keep this in someplace safe! \nIf you lose it you’ll no longer be able to decrypt files.\n")
+                print('Key: ', c)
+            except FileNotFoundError:
+                continue
+
+
+        elif event == 'Decrypt':
+            try:
+                with open(filename, 'rb') as f:
+                    data = f.read()
+                salt = os.urandom(16)
+                kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=100000, backend=default_backend())
+                password = passw.encode()
+                file = filename.encode()
+                key = base64.urlsafe_b64encode(kdf.derive(password))
+                c = key.decode()
+                fernet = Fernet(key)
+                encrypted = fernet.encrypt(data)
+                file = open('key.docx', 'rb')
+                keyt = file.read() 
+                file.close()
+                fernet = Fernet(keyt)
+                encrypted = fernet.decrypt(data)
+                with open(filename, 'wb') as f:
+                    f.write(encrypted)
+
+                print('File decrypted... ')
+            except FileNotFoundError:
+                continue
+
+
+        elif event == 'About...':
+            sg.popup('About:', 'Created by A. Petek', 'Crypter', 'Version 1.1',)
+
+        elif event == 'paypal':
+            webbrowser.open_new_tab("https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=PFB6A6HLAQHC2&source=url")
+
+        
+        elif event == 'Settings':
+            event, values = create_settings_window(settings).read(close=True)
+            if event == 'Save':
+                window.close()
+                window = None
+                save_settings(SETTINGS_FILE, settings, values)
+
+        elif event == 'Exit':
+            break
+    window.Close()
+
+main()
---
+++ readme.md
@@ -0,0 +1,68 @@
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+
+# Crypter
+
+**Programmed in Python | PySimpleGUI**
+
+[![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/adrijano/Crypter-v1.1/)
+
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://github.com/adrijano/Crypter-v1.1/graphs/commit-activity)
+
+# If you like it give it a star
+
+[![GitHub stars](https://img.shields.io/github/stars/adrijano/crypto-watcher.svg?style=social&label=Star&maxAge=2592000)](https://github.com/adrijano/Crypter-v1.1)
+
+![Crypter](crypter.gif)
+
+# How it works
+```
+Crypter program use Fernet for encryption.
+Fernet guarantees that a message encrypted using it cannot be manipulated or read without the key. 
+Fernet is an implementation of symmetric (also known as “secret key”) 
+authenticated cryptography.
+
+Generates a fresh fernet key. Keep this some place safe! 
+If you lose it you’ll no longer be able to decrypt files.
+
+```
+# How to use
+
+### Python3+
+
+```
+git clone https://github.com/adrijano/Crypter-v1.1.git
+
+cd Crypter-v1.1 && pip install -r requirements.txt
+
+python crypter.py
+```
+### Windows
+```
+cd dist 
+crypter.exe
+```
+![Adrijan's github stats](https://github-readme-stats.vercel.app/api?username=adrijano&show_icons=true)
+
+## Donations
+If you would like to support me, donations are very welcome.
+
+```
+You can use Paypal to donate using your own credit card. 
+The payment is processed by PayPal but you don't need to have a
+PayPal account or sign-up for one if you are paying by credit card.
+
+You can also use your own Paypal account to donate.
+```
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=PFB6A6HLAQHC2&source=url)
+
+[![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)](https://github.com/adrijano/Crypter-v1.1/)
+
+
+# Disclaimer
+
+
+**The code within this repository comes with no guarantee, the use of this code is your responsibility. I take NO responsibility and/or liability for how you choose to use any of the source code available here. By using any of the files available in this repository, you understand that you are AGREEING TO USE AT YOUR OWN RISK. Once again, ALL files available here are for EDUCATION and/or RESEARCH purposes ONLY.**
+
+
+
+
